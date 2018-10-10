757881034bfc51eec6294925fe4065a5470bbb2c
pramot <endwindows07@gmail.com>
2018-10-10 15:23:56 +0700

    add order service

--- src/app/interfaces/change-password.interface.ts
+++ src/app/interfaces/change-password.interface.ts
@@ -1,5 +1,4 @@
 export interface IChangePassword {
     new_Password: string;
     old_Password: string;
-
 }
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,32 +1,39 @@
 import { Injectable } from "@angular/core";
 import { HttpService } from "./http.service";
-
+import { IOrderList } from "../interfaces/order/order-list.interface";
+declare let $;
 @Injectable()
 
 export class OrderService {
     constructor(private http: HttpService) { }
     
-    onGetOrders(){
-
+    onGetOrders(accessToken: string){
+        return this.http.requestPost(`api/Order/orders`, accessToken)
+            .toPromise() as Promise<IOrderList[]>;
     }
 
-    onGetOrderById(){
-
+    onGetOrderById(id: any, accessToken: string){
+        return this.http.requestPost(`api/Order/orders/${id}`,accessToken)
+            .toPromise() as Promise<IOrderList>;
     }
 
-    onInsertOrder(accessToken: string){
-        // return this.http.requestPost(`api/Order/insert-order`)
+    onInsertOrder(model: IOrderList, accessToken: string){
+        return this.http.requestPost(`api/Order/insert-order`, model, accessToken)
+                        .toPromise() as Promise<IOrderList>;
     }
 
-    onUpdateOrderById(){
-
+    onUpdateOrderById(model: IOrderList,id: any, accessToken: string){
+        return this.http.requestPost(`api/Order/update-order/${id}`,model, accessToken)
+            .toPromise() as Promise<IOrderList>;
     }
 
-    onAdjustOrderById(){
-
+    onAdjustOrderById(id: any, accessToken: string) {
+        return this.http.requestPost(`api/Order/adjust-order/${id}`, accessToken)
+            .toPromise() as Promise<IOrderList>;
     }
 
-    onCancelOrderById(){
-
+    onCancelOrderById(id: any, accessToken: string) {
+        return this.http.requestPost(`api/Order/cancel-order/${id}`, accessToken)
+            .toPromise() as Promise<IOrderList>;
     }
 }
