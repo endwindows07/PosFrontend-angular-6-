f7b599469e3d9d2d078307e7a2c2266960fb8a94
pramot <endwindows07@gmail.com>
2018-10-16 22:19:20 +0700

    show top product sales

--- src/app/interfaces/report/report-bestsales.interface.ts
+++ src/app/interfaces/report/report-bestsales.interface.ts
@@ -1,4 +1,5 @@
 export interface IBestSales {
     product_TotalSales: string;
     productId: string;
+    product_Name: string;
 }
--- src/app/report/components/product-bestsales/product-bestsales.component.ts
+++ src/app/report/components/product-bestsales/product-bestsales.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ReportService } from 'src/app/services/report.service';
+import { Router } from '@angular/router';
+import { AlertService } from 'src/app/layout/components/services/alert.service';
+import { AccessTokenService } from 'src/app/services/accesstoken.service';
+import { ISearchOption } from 'src/app/interfaces/search-option.interface';
+import { IBestsalesList } from 'src/app/interfaces/report/report.bestsalesList.interface';
 
 @Component({
   selector: 'app-product-bestsales',
@@ -7,6 +13,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductBestsalesComponent {
 
-  constructor() { }
+  constructor(
+    private reportService: ReportService,
+    private router: Router,
+    private alert: AlertService,
+    private accessTokenService: AccessTokenService, 
+  ) {
+    this.onInitailloadProductBestSales({
+      Start_Page: this.Start_Page,
+      Limit_Page: this.Limit_Page,
+      Search_DefaultText: this.Search_DefaultText,
+      Search_DefaultType: this.Search_DefaultType
+    });
+   }
+  Start_Page: number = 1;
+  Limit_Page: number = 10;
 
+  Search_Text: string;
+  Search_Type: string;
+
+  productBestSalesList: IBestsalesList ;
+
+  Search_DefaultType: string = 'SalesNow';
+  Search_DefaultText: string = "SalesNow";
+  // SalesTime 
+
+  onInitailloadProductBestSales(option: ISearchOption){
+    this.reportService.onGetProductBestSales(option, this.accessTokenService.getAccesstokenStore())
+         .then(bestSalesList => {
+           this.productBestSalesList = bestSalesList;
+           console.log(bestSalesList);
+         })
+         .catch(err => {
+          this.alert.error_alert(err.Message);
+         });
+  }
 }
